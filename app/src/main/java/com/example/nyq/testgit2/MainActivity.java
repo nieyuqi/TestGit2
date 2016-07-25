19cf7058cfc32c83d10c5c06722c8bbfb4aeed68
nyq <940725773@qq.com>
2016-07-25 20:38:19 +0800

    02

---
+++ app/src/main/java/com/example/nyq/testgit2/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.nyq.testgit2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int a =0;
+        int b =1;
+        int b2 =1;
+        int c ;
+        int d ;
+
+
+    }
+
+    public void t() {
+
+    }
+}
